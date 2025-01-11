9d3beca317709573e606f82883e24db7a30b47c9
Yulia Samoylova <u.u.samoylova@gmail.com>
2025-01-12 01:09:07 +0500

    update tests category

--- tests/test_category.py
+++ tests/test_category.py
@@ -7,8 +7,7 @@ def test_init_1(category_1):
     assert category_1.products == ["Samsung Galaxy S23 Ultra", "Iphone 15", "Xiaomi Redmi Note 11"]
     assert category_1.product_count == 1
     assert category_1.category_count == 1
-
-
+    assert len(category_1.products) == 3
 
 
 
