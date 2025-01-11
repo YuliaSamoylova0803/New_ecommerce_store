0686cca98815ac5fe4828356eea35a6261afe482
Yulia Samoylova <u.u.samoylova@gmail.com>
2025-01-11 23:55:32 +0500

    update module product

--- src/product.py
+++ src/product.py
@@ -7,9 +7,12 @@ class Product:
     price: float       # цена
     quantity: str      # количество в наличии
 
+
+
     def __init__(self, name, description, price, quantity):
         """Метод для инициализации экземпляра класса. Задаем значения атрибутам экземпляра."""
         self.name = name
         self.description = description
         self.price = price
         self.quantity = quantity
+
