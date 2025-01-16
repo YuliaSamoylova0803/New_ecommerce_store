f83435ae893f997405a1dee121053da214cad80c
Yulia Samoylova <u.u.samoylova@gmail.com>
2025-01-16 23:53:57 +0500

    update module category

--- src/category.py
+++ src/category.py
@@ -1,5 +1,6 @@
 from src.product import Product
 
+
 class Category:
     """Класс для представления продуктов"""
 
@@ -19,11 +20,9 @@ class Category:
         Category.category_count += 1
         Category.product_count += len(self.products) if products else 0
 
-
     def add_product(self, product: Product):
         self.__products.append(product)
         Category.product_count += 1
-
 
     @property
     def products(self):
@@ -33,25 +32,23 @@ class Category:
         return product_str
 
     @property
-    def products(self):
+    def products(self) -> list:
         return self.__products
 
 
-if __name__ == "__main__":
-    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
-    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
-    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
-
-    category1 = Category(
-        "Смартфоны",
-        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
-        [product1, product2, product3]
-    )
-
-    print(category1.products)
-    product4 = Product("55\" QLED 4K", "Фоновая подсветка", 123000.0, 7)
-    category1.add_product(product4)
-    print(category1.products)
-    print(category1.product_count)
-
-
+# if __name__ == "__main__":
+#     product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+#     product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+#     product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+#
+#     category1 = Category(
+#         "Смартфоны",
+#         "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+#         [product1, product2, product3]
+#     )
+#
+#     print(category1.products)
+#     product4 = Product("55\" QLED 4K", "Фоновая подсветка", 123000.0, 7)
+#     category1.add_product(product4)
+#     print(category1.products)
+#     print(category1.product_count)
