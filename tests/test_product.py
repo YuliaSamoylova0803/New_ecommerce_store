8c0a9a521cba514f7ef81cd8df5ff1d36d52f722
Yulia Samoylova <u.u.samoylova@gmail.com>
2025-01-16 23:54:56 +0500

    update files tests

--- tests/test_product.py
+++ tests/test_product.py
@@ -1,3 +1,8 @@
+from unittest.mock import patch
+
+from src.product import Product
+
+
 def test_init_1(product_1):
     assert product_1.name == "Samsung Galaxy S23 Ultra"
     assert product_1.description == "256GB, Серый цвет, 200MP камера"
@@ -24,3 +29,17 @@ def test_init_4(product_4):
     assert product_4.description == "Фоновая подсветка"
     assert product_4.price == 123000.0
     assert product_4.quantity == 7
+
+
+def test_new_product_method(dict_test):
+    assert str(Product.new_product(dict_test)) == "Iphone 15, 210000.0 руб. 8: 15 шт."
+
+
+def test_str_method(product_3):
+    product1 = product_3
+    assert str(product1) == "Xiaomi Redmi Note 11, 31000.0 руб. 14: 15 шт."
+
+
+@patch("src.product.input")
+def test_product_price_setter(mock_input):
+    mock_input.return_value = "Цена понижается с 180000.0 по 800. Подтверждаете? (y/n)"
