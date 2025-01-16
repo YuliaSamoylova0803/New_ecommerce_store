459248d8b98f4e8470caed96d1c960dad79d4717
Yulia Samoylova <u.u.samoylova@gmail.com>
2025-01-17 00:00:59 +0500

    update tests category

--- tests/test_category.py
+++ tests/test_category.py
@@ -4,10 +4,7 @@ from src.product import Product
 
 def test_init_1(category_1):
     assert category_1.name == "Смартфоны"
-    assert (
-        category_1.description
-        == "Смартфоны, как средство для получения дополнительных функций для удобства жизни"
-    )
+    assert category_1.description == "Смартфоны, как средство для получения дополнительных функций для удобства жизни"
     assert category_1.products == ["Samsung Galaxy S23 Ultra", "Iphone 15", "Xiaomi Redmi Note 11"]
     assert category_1.product_count == 3
     assert category_1.category_count == 1
