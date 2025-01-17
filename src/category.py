dd75dd60391c63f2970f570302231f2992c46f8d
Yulia Samoylova <u.u.samoylova@gmail.com>
2025-01-17 16:59:21 +0500

    update module category

--- src/category.py
+++ src/category.py
@@ -20,6 +20,14 @@ class Category:
         Category.category_count += 1
         Category.product_count += len(self.products) if products else 0
 
+    def __str__(self):
+        total_products = 0
+        for product in self.__products:
+            total_products += product.quantity
+        return f"{self.name}, количество продуктов: {total_products} шт."
+
+
+
     def add_product(self, product: Product):
         self.__products.append(product)
         Category.product_count += 1
