8747fbdb7d6e0357c3c8027b0edac20828293c7f
Yulia Samoylova <u.u.samoylova@gmail.com>
2025-01-18 23:25:58 +0500

    update module category iterator

---
+++ src/category_iterator.py
@@ -0,0 +1,44 @@
+
+class CategoryIterator:
+    """Дополнительный класс для перебора продуктов"""
+
+    def __init__(self, category):
+        self.category = category
+        self.index = 0
+
+    def __iter__(self):
+        self.index = 0
+        return self
+
+    def __next__(self):
+        if self.index < len(self.category.products):
+            product = self.category.products[self.index]
+            self.index += 1
+            return product
+        else:
+            raise StopIteration
+
+
+# if __name__ == "__main__":
+#     product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+#     product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+#     product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+#
+#     print(str(product1))
+#     print(str(product2))
+#     print(str(product3))
+#
+#     category1 = Category(
+#         "Смартфоны",
+#         "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+#         [product1, product2, product3],
+#     )
+#
+#     print(str(category1))
+#
+#     print(category1.products)
+#
+#     iterator = CategoryIterator(category1)
+#
+#     for category1 in iterator:
+#         print(category1)
