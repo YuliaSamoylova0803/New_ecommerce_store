66674deed29a754ad473558f46da9d666ca8a035
Yulia Samoylova <u.u.samoylova@gmail.com>
2025-01-30 14:25:44 +0500

    update module category

--- src/category.py
+++ src/category.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 
+from src.exceptions import ZeroProduct
 from src.product import Product
 
 
@@ -51,9 +52,17 @@ class Category(Abstract):
 
     def add_product(self, new_product):
         if isinstance(new_product, Product):
-
-            self.__products.append(new_product)
-            Category.product_count += 1
+            try:
+                if new_product.quantity == 0:
+                    raise ZeroProduct("Нельзя добавить товар с нулевым количеством")
+            except ZeroProduct as e:
+                print(str(e))
+            else:
+                self.__products.append(new_product)
+                Category.product_count += 1
+                print("Товар добавлен успешно")
+            finally:
+                print("Обработка добавления товара завершена")
         else:
             raise TypeError
 
@@ -68,39 +77,44 @@ class Category(Abstract):
     def products(self) -> list:
         return self.__products
 
+    def middle_price(self):
+        try:
+            return round(sum([product.price for product in self.__products]) / len(self.__products), 2)
+        except ZeroDivisionError:
+            return 0
+
 
 # if __name__ == "__main__":
-#     product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
-#     ord_1 = Order(product2, 10)
-#     print(ord_1)
-#
-#     product_1 = Product("tomato", "red tomato from Azerbaijan", 150, 10)
-#     order = Order(product_1, 20)
-#     print(order)
-# message = capsys.readouterr()
-# assert message.out.strip().split("\n")[-1] == "Product('tomato', 'red tomato from Azerbaijan', 150, 10)"
-# assert repr(order) == "tomato, куплено - 20 штук, итоговая стоимость - 3000 рублей""Samsung Galaxy S23 Ultra"
+#     # product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+#     # ord_1 = Order(product2, 10)
+#     # print(ord_1)
+#     #
+#     # product_1 = Product("tomato", "red tomato from Azerbaijan", 150, 10)
+#     # order = Order(product_1, 20)
+#     # print(order)
+#     #
 #     product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
 #     product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
 #     product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
-#
+#     #
 #     category1 = Category(
 #         "Смартфоны",
 #         "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
 #         [product1, product2, product3]
-#     )
-#     cat_2 = Category(
-#         name="Смартфоны",
-#         description="Смартфоны, как средство для получения дополнительных функций для удобства жизни",
-#         products=["Samsung Galaxy S23 Ultra", "Iphone 15", "Xiaomi Redmi Note 11"],
-#     )
-#     print(cat_2)
+#      )
+#     # cat_2 = Category(
+#     #     name="Смартфоны",
+#     #     description="Смартфоны, как средство для получения дополнительных функций для удобства жизни",
+#     #     products=["Samsung Galaxy S23 Ultra", "Iphone 15", "Xiaomi Redmi Note 11"],
+#     # )
+#     # #print(cat_2)
 #     # print(category1.products)
-#     # product4 = Product("55\" QLED 4K", "Фоновая подсветка", 123000.0, 7)
-#     # category1.add_product(product4)
-#     # print(category1.products)
-#     # print(category1.product_count)
-#     # product_1 = Product("tomato", "red tomato from Azerbaijan", 150, 10)
-#     # category_1 = Category("products", "products for salad", [])
-#     # category_1.add_product(product_1)
-#     print(category1)
+#     product4 = Product("55\" QLED 4K", "Фоновая подсветка", 123000.0, 7)
+#     product5 = Product("55\" QLED 4K", "Фоновая подсветка", 0, 0)
+#     category1.add_product(product5)
+#     print(category1.products)
+#     print(category1.product_count)
+# product_1 = Product("tomato", "red tomato from Azerbaijan", 150, 10)
+# category_1 = Category("products", "products for salad", [])
+# category_1.add_product(product_1)
+# print(category1)
